49048a9c84472010db746e4c7d234f7c736d2480
plebea <bini060315kim@gmail.com>
2022-07-15 08:53:34 +0900

    add: api.js

--- api.js
+++ api.js
@@ -4,3 +4,23 @@ const getAPI = axios.create({
     withCredentials: true,
     baseURL: process.env.API_URL,
 })
+
+export async function apiRegister(data){
+    await getAPI.post(`/auth/new`, data).then((res) => res.data).catch((e) => console.log(e))
+}
+
+export async function apiLogin(data){ 
+    await getAPI.post(`/auth/signin`, data).then((res) => res.data).catch((e) => console.log(e))
+}
+
+export async function apiLogout() {
+    getAPI.defaults.headers.common['Authorization'] = null
+    await getAPI.delete(`/auth/logout`).then((res) => res.data).catch((e) => console.log(e))
+}
+
+export async function getAuthToken() {
+    await getAPI.get(`/auth/signin`).then((res) => {
+        getAPI.defaults.headers.common['Authorization'] = res.data.authToken
+        return res.data
+    })
+}
