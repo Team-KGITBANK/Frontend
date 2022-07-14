b85e3af4af2da96a81c6d1b58a230c6585085f77
plebea <bini060315kim@gmail.com>
2022-07-15 08:54:29 +0900

    create: login page

--- pages/login.vue
+++ pages/login.vue
@@ -14,13 +14,16 @@
             <a href="/register" class="login_signin">Sign in</a>
         </div>
         <div class="login_btn_wrapper">
-            <button class="login_btn">Login</button>
+            <button class="login_btn" v-on:click="login">Login</button>
+            <p class="register_warn_message">{{ warn_message }}</p>
         </div>
     </div>
   </div>
 </template>
 
 <script>
+const { apiLogin, getAuthToken } = require('@/api.js')
+
 export default {
     name: "LoginPage",
     data() {
@@ -28,8 +31,31 @@ export default {
             id: "",
             pw: "",
             remember: false,
+            warn_message: ""
         }
     },
+    methods: {
+        async login() {
+            if (!this.id || !this.pw) { this.warn_message = "모든 칸을 입력해주세요" }
+            else{ 
+                apiLogin({
+                    id: this.id,
+                    password: this.pw,
+                }).then(()=>{
+                    if (this.remember) {
+                        localStorage.setItem("id", this.id)
+                        localStorage.setItem("pw", this.pw)
+                    }
+                    getAuthToken().then(token => {
+                        localStorage.setItem("token", token)
+                        this.$router.push("/")
+                    })
+                }).catch(()=>{
+                    this.warn_message = "아이디 또는 비밀번호가 올바르지 않습니다"
+                })
+            }
+        }
+    }
 }
 </script>
 
