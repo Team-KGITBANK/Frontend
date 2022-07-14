712124f81f84e4a8381a31c6a73d52fb5af7afb0
plebea <bini060315kim@gmail.com>
2022-07-14 23:09:13 +0900

    create: register page

--- pages/register.vue
+++ pages/register.vue
@@ -1,11 +1,70 @@
 <template>
-  <div class="register">
+  <div class="login">
     <div class="register_box">
-        <h1 class="login_title">User Loin</h1>
-        <div class="login-input-box">
-            <input class="login_input" type="text" v-model="id">
-            <input class="login_input" type="password" v-model="pw">
-            <input class="login_input" type="password" v-model="pw_check">
+        <h1 class="register_title">Sign in</h1>
+        <div class="login_input_box">
+            <div class="register_all_input">
+                <div class="register_name_wrapper">
+                    <div class="register_input_wrapper">
+                        <div class="register_input_text_wrapper">
+                            <p class="register_text">First name</p>
+                            <p style="color:red;">*</p>
+                        </div>
+                        <input type="text" class="register_input" v-model="first_name">
+                    </div>
+                    <div class="register_input_wrapper">
+                        <div class="register_input_text_wrapper">
+                            <p class="register_text">Last name</p>
+                            <p style="color:red;">*</p>
+                        </div>
+                        <input type="text" class="register_input" v-model="last_name">
+                    </div>
+                </div>
+                <div class="register_email">
+                    <div class="register_input_wrapper">
+                        <div class="register_input_text_wrapper">
+                            <p class="register_text">Email</p>
+                            <p style="color:red;">*</p>
+                        </div>
+                        <input type="text" class="register_input" v-model="email">
+                    </div>
+                </div>
+                <div class="register_name_wrapper">
+                    <div class="register_input_wrapper">
+                        <div class="register_input_text_wrapper">
+                            <p class="register_text">ID</p>
+                            <p style="color:red;">*</p>
+                        </div>
+                        <input type="text" class="register_input" v-model="id">
+                    </div>
+                    <div class="register_input_wrapper">
+                        <div class="register_input_text_wrapper">
+                            <p class="register_text">Phone number</p>
+                            <p style="color:red; opacity: 0;">*</p>
+                        </div>
+                        <input type="text" class="register_input" v-model="phone_number">
+                    </div>
+                </div>
+                <div class="register_name_wrapper">
+                    <div class="register_input_wrapper">
+                        <div class="register_input_text_wrapper">
+                            <p class="register_text">Password</p>
+                            <p style="color:red;">*</p>
+                        </div>
+                        <input type="password" class="register_input" v-model="pw">
+                    </div>
+                    <div class="register_input_wrapper">
+                        <div class="register_input_text_wrapper">
+                            <p class="register_text">Password Check</p>
+                            <p style="color:red;">*</p>
+                        </div>
+                        <input type="password" class="register_input" v-model="pw_check">
+                    </div>
+                </div>
+            </div>
+            <div class="register_submit_wrapper">
+                <button class="register_submit">Submit</button>
+            </div>
         </div>
     </div>
   </div>
@@ -14,11 +73,15 @@
 <script>
 export default {
     name: "RegisterPage",
-    date() {
+    data() {
         return {
             id: "",
             pw: "",
-            pw_check: ""
+            pw_check: "",
+            first_name: "",
+            last_name: "",
+            email: "",
+            phone_number: "",
         }
     },
     methods: {
