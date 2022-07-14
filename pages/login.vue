c37cfb75722b7e3746fa190c36d3d70ba0970aa4
plebea <bini060315kim@gmail.com>
2022-07-14 23:09:00 +0900

    create: login page

--- pages/login.vue
+++ pages/login.vue
@@ -2,9 +2,19 @@
   <div class="login">
     <div class="login_box">
         <h1 class="login_title">User Loin</h1>
-        <div class="login-input-box">
+        <div class="login_input_box">
             <input class="login_input" type="text" v-model="id" placeholder="ID">
             <input class="login_input" type="password" v-model="pw" placeholder="Password">
+        </div>
+        <div class="login_btn_box">
+            <div class="login_remember">
+                <input type="checkbox" v-model="remember">
+                <p>Remember me</p>
+            </div>
+            <a href="/register" class="login_signin">Sign in</a>
+        </div>
+        <div class="login_btn_wrapper">
+            <button class="login_btn">Login</button>
         </div>
     </div>
   </div>
@@ -16,7 +26,8 @@ export default {
     data() {
         return{
             id: "",
-            pw: ""
+            pw: "",
+            remember: false,
         }
     },
 }
